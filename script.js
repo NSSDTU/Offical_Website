3864d3674bc88c1315ce3088c92caf1ca8d62623
Tushar Mishra <tusharmishra020@gmail.com>
2021-10-16 15:36:17 +0530

    minpr

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ setTimeout(fade, 1000);
 window.addEventListener('scroll',()=>{
 let value = window.scrollY;
 
-if(value>=700){
+if(value>=4){
     document.getElementById("navbar").style.backgroundColor="rgb(44, 44, 44)"
 }else{
     document.getElementById("navbar").style.backgroundColor="transparent"
