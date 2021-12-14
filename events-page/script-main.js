845fd1f6e388399f58f48fee0e96936b024c308c
Harshal Chowdhary <76219199+CODERGU7@users.noreply.github.com>
2021-12-14 13:07:53 +0530

    Update script-main.js

--- events-page/script-main.js
+++ events-page/script-main.js
@@ -1,3 +1,15 @@
+const navMenu = document.getElementById('nav-menu'),
+toggleMenu = document.getElementById('toggle-menu'),
+closeMenu = document.getElementById('close-menu');
+
+toggleMenu.addEventListener('click',()=>{
+    navMenu.classList.toggle('show')
+})
+
+closeMenu.addEventListener('click',()=>{
+    navMenu.classList.remove('show')
+})
+
 var headpara = document.getElementById("head3");
 function fade(){
     headpara.style.opacity="100%";
